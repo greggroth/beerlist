a9fc34e134ed001058c2f78cc0b3693b1a1e6dda
Greggory Rothmeier <greggroth@gmail.com>
2011-08-11 17:10:57 -0400

    added a hover() color to show that the rows are expandable

--- public/javascripts/jExpand.js
+++ public/javascripts/jExpand.js
@@ -7,14 +7,18 @@
         $(element).find("tr:not(.odd)").hide();
         $(element).find("tr:first-child").show();
 
+		$(element).find("tr.odd").hover(function() {
+			$(this).toggleClass("active")
+		});
+
         $(element).find("tr.odd").click(function() {
         	var url = $(this).attr('title') + " .itemlist";
         	var item_id = "#itemdesc-" + $(this).attr('id');
         	
-        	        	$(this).next("tr").toggle();
+        	$(this).next("tr").toggle();
         	
         	if ($(this).next("tr").is(":visible")) {
-        		$(item_id).children('p').hide().html('loading...').load(url).slideDown();
+        		$(item_id).children('p').hide().css('border','solid thin #E8E8E8').html('loading...').load(url).show();
         	};
         });
         
