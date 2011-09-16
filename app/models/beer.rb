8b1f19a1a7cdee5ed603dd8ebc4e0aa74835ec56
Greggory Rothmeier <greggroth@gmail.com>
2011-09-16 15:14:11 -0400

    trying to add beer style to serarch restuls on the beer/index page

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,20 +2,36 @@ class Beer < ActiveRecord::Base
 	validates_presence_of :name, :brewery_id
 	validates_uniqueness_of :name, :scope => :brewery_id, :message => " has already been added for this brewery"
 
+  belongs_to :brewery
+	belongs_to :beer_style
+	has_many :beer_items, :dependent => :destroy
+	has_many :bars, :through => :beer_items
+  
   has_paper_trail
 
   include Tanker
   
   tankit 'index' do
     indexes :name
+    indexes :style
+    
+    # # you may also dynamically retrieve field data
+    # indexes :beer_styles do
+    #   beer_style.map {|s| s.name }
+    # end
   end
+  
+  
+  def style
+    if self.try(:beer_style)
+      self.beer_style.name
+    else
+      "n/a"
+    end
+  end
+  
   
   after_save :update_tank_indexes
   after_destroy :delete_tank_indexes
-
-	belongs_to :brewery
-	belongs_to :beer_style
-	has_many :beer_items, :dependent => :destroy
-	has_many :bars, :through => :beer_items
   
 end
