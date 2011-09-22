be7a674703e289b04f37dae76472dc8e91334aaf
Greggory Rothmeier <greggroth@gmail.com>
2011-09-22 15:13:34 -0400

    test using the jqtouch goto method instead of href

--- public/javascripts/iphone.js
+++ public/javascripts/iphone.js
@@ -1,26 +1,24 @@
-$.jQTouch({
-
-});
+var jQT = new $.jQTouch();
 
 $(document).ready(function() {
 	var loading_message;
 	loarding_message = '<p id="loading-message">Loading...</p>';
 	
-	console.log("IPHONE.JS LOADED OK");
-	
 	$("a.button_bars_index").live("click", function() {
-		console.log("ButtonBarsIndex clicked");
 		$("div#bars_index_container").html(loarding_message).load("/bars");
+		jQT.goTo('#bars_index', 'slideleft');
 	});
 	
 	$("a.button_beers_index").live("click", function() {
 		$("div#beers_index_container").html(loarding_message).load("/beers");
+		jQT.goTo('#beers_index', 'slideleft');
 	});
 	
 	$("a.button_beer_items_show").live("click", function() {
 		var id;
 		id = $(this).attr("bi-id");
 		$("div#beer_items_show_container").html(loarding_message).load("/beer_items/" + id);
+		jQT.goTo('#beer_items_show', 'slideleft');
 	});
 	
 	$("a.button_beers_show").live("click", function() {
@@ -28,6 +26,7 @@ $(document).ready(function() {
 		id = $(this).attr("beer-id");
 		$("div#beers_show .toolbar h1").html($(this).attr("beer-name"));
 		$("div#beers_show_container").html(loarding_message).load("/beers/" + id);   //  Replace Title with Beer Name
+		jQT.goTo('#beers_show', 'slideleft');
 	});
 	
 	$("a.button_bars_show").live("click", function() {
@@ -35,6 +34,7 @@ $(document).ready(function() {
 		id = $(this).attr("bar-id");
 		$("div#bars_show .toolbar h1").html($(this).attr("bar-name"));
 		$("div#bars_show_container").html(loarding_message).load("/bars/" + id);     //  Replace Title with Bar Name
+		jQT.goTo('#bars_show', 'slideleft');
 	});
 	
 	$("a.button_breweries_show").live("click", function() {
@@ -42,5 +42,6 @@ $(document).ready(function() {
 		id = $(this).attr("brewery-id");
 		$("div#breweries_show .toolbar h1").html($(this).attr("brewery-name"));
 		$("div#breweries_show_container").html(loarding_message).load("/breweries/" + id);     //  Replace Title with brewery Name
+		jQT.goTo('#breweries_show', 'slideleft');
 	});
 });
