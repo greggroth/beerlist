04b5ee4e7662764c364ee3686f7e54c6bf84fb28
Greggory Rothmeier <greggroth@gmail.com>
2011-10-08 15:51:25 -0400

    added notice if invalid query

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,10 +1,10 @@
 class SearchResultsController < ApplicationController
 
 def index
-  if params[:q].present?
+  unless params[:q].match(/[a-zA-Z0-9]/).nil?
     @results = Tanker.search([Bar, Beer, Brewery], params[:q], :per_page => 20)
   else
-    redirect_to root_path
+    redirect_to root_path, :alert => "Invalid Query"
   end
 end
 
