596e9107c061df8cd53306fad3c41bb378df3e67
Greggory Rothmeier <greggroth@gmail.com>
2011-10-24 14:35:51 -0400

    user can mark a beer as had from the beers page and from beers/index

--- app/controllers/beer_tracks_controller.rb
+++ app/controllers/beer_tracks_controller.rb
@@ -1,14 +1,31 @@
 class BeerTracksController < ApplicationController
   def create
     current_user.drinks_it(params[:beer_track])
-      
-    @beer_item = BeerItem.find(params[:beer_item_id], :include => [{:beer => :ratings}])
+    
+    case params[:from]
+    when "bars_page"
+      @from = "bars_page"
+      @beer_item = BeerItem.find(params[:beer_item_id], :include => [{:beer => :ratings}])
+    when "beers_page"
+      @from = "beers_page"
+      @beer = Beer.find(params[:beer_track][:beer_id])
+    when "beers_index"
+      @from = "beers_index"
+      @beer = Beer.find(params[:beer_track][:beer_id])
+    end
   end
 
   def destroy
     bt = BeerTrack.find(params[:id])
     bt.destroy
-
-    @beer_item = BeerItem.find(params[:beer_item_id])
+    
+    case params[:from]
+    when "bars_page"
+      @from = "bars_page"
+      @beer_item = BeerItem.find(params[:beer_item_id])
+    when "beers_index"
+      @from = "beers_index"
+      @beer = bt.beer
+    end
   end
 end
