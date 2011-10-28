365fb93e8fb835b8d14c16714061c06113de9df5
Greggory Rothmeier <greggroth@gmail.com>
2011-10-27 21:03:53 -0400

    fixed migration

--- db/migrate/20111018142124_add_column_to_beer_tracks.rb
+++ db/migrate/20111018142124_add_column_to_beer_tracks.rb
@@ -1,5 +1,5 @@
 class AddColumnToBeerTracks < ActiveRecord::Migration
   def change
-    add_column :beer_tracks, :integer, :beer_item_id
+    add_column :beer_tracks, :beer_item_id, :integer
   end
 end
